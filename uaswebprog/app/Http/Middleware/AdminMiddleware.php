75627102d4dec4f1045b41a0f56fa17dfc08fb78
ptikelompok8 <willbertlian@gmail.com>
2024-11-08 12:35:56 +0700

    admin

---
+++ uaswebprog/app/Http/Middleware/AdminMiddleware.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Symfony\Component\HttpFoundation\Response;
+
+class AdminMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        // Check if the user is authenticated and has admin privileges
+        if (Auth::check() && Auth::user()->is_admin) {
+            return $next($request);
+        }
+
+        // Redirect to home page if the user is not an admin
+        return redirect('/dashboard');
+    }
+}
---
+++ uaswebprog/resources/views/admin/index.blade.php
@@ -0,0 +1,9 @@
+// In resources/views/admin/index.blade.php
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <h1>Welcome, Admin!</h1>
+        <p>This is your admin dashboard.</p>
+    </div>
+@endsection
