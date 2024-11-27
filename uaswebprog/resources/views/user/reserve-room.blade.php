38547e40c820e21dac5122da1f12b42daaa1d347
ifBeans <fabiandustin27@gmail.com>
2024-11-27 18:31:39 +0700

    reserve shit idk

--- uaswebprog/resources/views/user/reserve-room.blade.php
+++ uaswebprog/resources/views/user/reserve-room.blade.php
@@ -4,4 +4,36 @@
             {{ __('Reserve A Room!!!') }}
         </h2>
     </x-slot>
+
+    
+    
+    <div class="flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100 dark:bg-gray-900">
+        <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white dark:bg-gray-800 shadow-md overflow-hidden sm:rounded-lg">
+            <form method="POST" action="{{ route('admin.add_room_images') }}" enctype="multipart/form-data">
+                <label for="countries" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Select an option</label>
+                <select id="countries" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                    <option selected>Choose a room</option>
+                    <option value="1A">1A</option>
+                    <option value="1B">1B</option>
+                    <option value="2A">2A</option>
+                    <option value="2B">2B</option>
+                    <option value="2C">2C</option>
+                    <option value="2D">2D</option>
+                    <option value="3A">3A</option>
+                    <option value="3B">3B</option>
+                    <option value="3C">3C</option>
+                    <option value="3D">3D</option>
+                </select>
+
+                <div class="flex items-center justify-end mt-4">
+                    <x-button class="ms-4">
+                        {{ __('Reserve Room') }}
+                    </x-button>
+                </div>
+            </form>
+            
+        </div>
+    </div>
+
+
 </x-app-layout>
