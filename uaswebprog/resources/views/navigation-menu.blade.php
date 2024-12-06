42fdb5ef3efd289370d1c419b97c2e0db9803ff8
ptikelompok8 <willbertlian@gmail.com>
2024-12-06 15:45:01 +0700

    Update navigation-menu.blade.php

--- uaswebprog/resources/views/navigation-menu.blade.php
+++ uaswebprog/resources/views/navigation-menu.blade.php
@@ -172,95 +172,38 @@
             </x-responsive-nav-link>
         </div>
 
-        <div class="pt-2 pb-3 space-y-1">
-            <x-responsive-nav-link href="{{ route('fasilitas') }}" :active="request()->routeIs('fasilitas')">
-                {{ __('Facilities') }}
-            </x-responsive-nav-link>
-        </div>
-
-        <div class="pt-2 pb-3 space-y-1">
-            <x-responsive-nav-link href="{{ route('reserve-room') }}" :active="request()->routeIs('reserve-room')">
-                {{ __('Reserve a Room') }}
-            </x-responsive-nav-link>
-        </div>
-
-        <div class="pt-2 pb-3 space-y-1">
-            <x-responsive-nav-link href="{{ route('my_room') }}" :active="request()->routeIs('my_room')">
-                {{ __('My Room') }}
-            </x-responsive-nav-link>
-        </div>
-
-        <!-- Responsive Settings Options -->
-        <div class="pt-4 pb-1 border-t border-gray-200 dark:border-gray-600">
-            <div class="flex items-center px-4">
-                @if (Laravel\Jetstream\Jetstream::managesProfilePhotos())
-                    <div class="shrink-0 me-3">
-                        <img class="h-10 w-10 rounded-full object-cover" src="{{ Auth::user()->profile_photo_url }}"
-                            alt="{{ Auth::user()->name }}" />
-                    </div>
-                @endif
-
-                <div>
-                    <div class="font-medium text-base text-gray-800 dark:text-gray-200">{{ Auth::user()->name }}</div>
-                    <div class="font-medium text-sm text-gray-500">{{ Auth::user()->email }}</div>
-                </div>
-            </div>
-
-            <div class="mt-3 space-y-1">
-                <!-- Account Management -->
-                <x-responsive-nav-link href="{{ route('profile.show') }}" :active="request()->routeIs('profile.show')">
-                    {{ __('Profile') }}
+        @guest
+            <div class="pt-2 pb-3 space-y-1">
+                <x-responsive-nav-link href="{{ route('fasilitas') }}" :active="request()->routeIs('fasilitas')">
+                    {{ __('Facilities') }}
                 </x-responsive-nav-link>
-
-                @if (Laravel\Jetstream\Jetstream::hasApiFeatures())
-                    <x-responsive-nav-link href="{{ route('api-tokens.index') }}" :active="request()->routeIs('api-tokens.index')">
-                        {{ __('API Tokens') }}
-                    </x-responsive-nav-link>
-                @endif
-
-                <!-- Authentication -->
-                <form method="POST" action="{{ route('logout') }}" x-data>
-                    @csrf
-
-                    <x-responsive-nav-link href="{{ route('logout') }}" @click.prevent="$root.submit();">
-                        {{ __('Log Out') }}
-                    </x-responsive-nav-link>
-                </form>
-
-                <!-- Team Management -->
-                @if (Laravel\Jetstream\Jetstream::hasTeamFeatures())
-                    <div class="border-t border-gray-200 dark:border-gray-600"></div>
-
-                    <div class="block px-4 py-2 text-xs text-gray-400">
-                        {{ __('Manage Team') }}
-                    </div>
-
-                    <!-- Team Settings -->
-                    <x-responsive-nav-link href="{{ route('teams.show', Auth::user()->currentTeam->id) }}"
-                        :active="request()->routeIs('teams.show')">
-                        {{ __('Team Settings') }}
-                    </x-responsive-nav-link>
-
-                    @can('create', Laravel\Jetstream\Jetstream::newTeamModel())
-                        <x-responsive-nav-link href="{{ route('teams.create') }}" :active="request()->routeIs('teams.create')">
-                            {{ __('Create New Team') }}
-                        </x-responsive-nav-link>
-                    @endcan
-
-                    <!-- Team Switcher -->
-                    @if (Auth::user()->allTeams()->count() > 1)
-                        <div class="border-t border-gray-200 dark:border-gray-600"></div>
-
-                        <div class="block px-4 py-2 text-xs text-gray-400">
-                            {{ __('Switch Teams') }}
-                        </div>
-
-                        @foreach (Auth::user()->allTeams() as $team)
-                            <x-switchable-team :team="$team" component="responsive-nav-link" />
-                        @endforeach
-                    @endif
-                @endif
             </div>
-        </div>
+
+            <div class="pt-2 pb-3 space-y-1">
+                <x-responsive-nav-link href="{{ route('reserve-room') }}" :active="request()->routeIs('reserve-room')">
+                    {{ __('Reserve a Room') }}
+                </x-responsive-nav-link>
+            </div>
+        @endguest
+
+        @auth
+            <div class="pt-2 pb-3 space-y-1">
+                <x-responsive-nav-link href="{{ route('fasilitas') }}" :active="request()->routeIs('fasilitas')">
+                    {{ __('Facilities') }}
+                </x-responsive-nav-link>
+            </div>
+
+            <div class="pt-2 pb-3 space-y-1">
+                <x-responsive-nav-link href="{{ route('reserve-room') }}" :active="request()->routeIs('reserve-room')">
+                    {{ __('Reserve a Room') }}
+                </x-responsive-nav-link>
+            </div>
+
+            <div class="pt-2 pb-3 space-y-1">
+                <x-responsive-nav-link href="{{ route('my_room') }}" :active="request()->routeIs('my_room')">
+                    {{ __('My Room') }}
+                </x-responsive-nav-link>
+            </div>
+        @endauth
     </div>
 </nav>
